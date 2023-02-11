00154edc301bedfad75ed1be31573ff3cf171eb4
takunnithan <takunnithan@gmail.com>
2023-02-11 18:22:51 +0100

    Final

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type Link struct {
+	Href, Text string
+}
+
+func main() {
+	fmt.Println("HTML link parser!")
+	file_ptr, err := os.Open("E:\\Go\\go projects\\html_link_parser\\ex2.html")
+	if err != nil {
+		panic(err)
+	}
+	var links []Link
+	html_tokenizer := html.NewTokenizer(file_ptr)
+	for {
+		tt := html_tokenizer.Next()
+		token := html_tokenizer.Token()
+		if tt == html.ErrorToken {
+			break
+		}
+		if tt == html.StartTagToken && token.DataAtom.String() == "a" {
+			attrs := token.Attr
+			var url string
+			for _, attr := range attrs {
+				if attr.Key == "href" {
+					url = attr.Val
+					break
+				}
+			}
+			texts := getLinkTexts(html_tokenizer)
+			link := Link{Href: url, Text: texts}
+			links = append(links, link)
+		}
+	}
+	val, err := json.Marshal(links)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(val))
+}
+
+func getLinkTexts(html_tokenizer *html.Tokenizer) string {
+	var texts []string
+	for {
+		tag := html_tokenizer.Next()
+		token := html_tokenizer.Token()
+		if tag == html.EndTagToken && token.DataAtom.String() == "a" {
+			break
+		}
+		if tag == html.TextToken {
+			texts = append(texts, strings.Fields(token.Data)...)
+		}
+	}
+	return strings.Join(texts, " ")
+}
